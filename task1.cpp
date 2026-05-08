5bfb1a6925ab53a74b9fb1a080dc4d2f7ab429b6
lxhykc <lxhykc@gmail.com>
2026-05-08 12:45:57 +0200

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -3,5 +3,9 @@ using namespace std;
 int main() {
 cout << "Hello GitHub!" << endl;
 cout << "This is my first C++ file." << endl;
+  cout << "I am learning C++ and GitHub." << endl;
+cout << "This is my second commit." << endl;
+  cout << "I downloaded this project and edited it on my computer." << endl;
+cout << "Now I will upload it again to GitHub." << endl;
 return 0;
 }
